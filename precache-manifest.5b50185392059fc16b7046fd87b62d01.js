a2412a7c2273b41e8009d9b929df3dac80091da9
arnavigode <arnavi.gode96@gmail.com>
2022-01-11 20:28:50 +0530

    Updates

---
+++ precache-manifest.5b50185392059fc16b7046fd87b62d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c294f917f3ae3f13a7b383a4ce659cac",
+    "url": "/portfolioupload/index.html"
+  },
+  {
+    "revision": "87f7ccc0c4476035348b",
+    "url": "/portfolioupload/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "2a06a9c75ae62257ab03",
+    "url": "/portfolioupload/static/css/main.307024db.chunk.css"
+  },
+  {
+    "revision": "87f7ccc0c4476035348b",
+    "url": "/portfolioupload/static/js/2.baa6302a.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/portfolioupload/static/js/2.baa6302a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a06a9c75ae62257ab03",
+    "url": "/portfolioupload/static/js/main.fcd1105b.chunk.js"
+  },
+  {
+    "revision": "31001d205668ecf4afe7",
+    "url": "/portfolioupload/static/js/runtime-main.e760e0b9.js"
+  }
+]);
